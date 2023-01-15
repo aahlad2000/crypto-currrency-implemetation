55e3699654b90c44fb9927afb656a2a279da5f12
Aahlad <aahladkethineeedi@gmail.com>
2023-01-15 21:22:04 +0530

    Added main.kt

--- src/main/kotlin/kotlinCoin/Utils.kt
+++ src/main/kotlin/kotlinCoin/Utils.kt
@@ -10,13 +10,13 @@ fun String.hash(algorithm: String = "SHA-256"): String{
     return String.format("%064x", BigInteger(1, messageDigest.digest()))
 }
 
-fun String.sign(privateKey: PrivateKey, algorithm: String="SHA-256withRSA"): ByteArray{
+fun String.sign(privateKey: PrivateKey, algorithm: String="SHA256withRSA"): ByteArray{
     val rsa = Signature.getInstance(algorithm)
     rsa.initSign(privateKey)
     rsa.update(this.toByteArray())
     return rsa.sign()
 }
-fun String.verifySignature(publicKey: PublicKey, signature: ByteArray, algorithm: String="SHA-256withRSA"): Boolean{
+fun String.verifySignature(publicKey: PublicKey, signature: ByteArray, algorithm: String="SHA256withRSA"): Boolean{
     val rsa = Signature.getInstance(algorithm)
     rsa.initVerify(publicKey)
     rsa.update(this.toByteArray())
